cf6d2e947623fdaeeb45d0ebde7aee7acdd95361
Ricard Fíguls Mateu <ricard.figuls@altran.com>
2020-12-14 14:08:09 +0100

    Change build system

---
+++ publish.js
@@ -0,0 +1,25 @@
+const execa = require("execa");
+const fs = require("fs");
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log("Building...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("build") ? "build" : "build";
+    fs.copyFileSync('./assets/index.html', './build/index.html');
+    fs.copyFileSync('./assets/index.css', './build/index.css');
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "Deploy to gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed");
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
